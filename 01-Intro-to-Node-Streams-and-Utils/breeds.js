627cf33643d0a806d1785cc07228ea77e4864c64
Boyan Yanchev <byanchev6@gmail.com>
2026-06-22 23:25:27 +0300

    feat: use uuid v4 for id generation

--- 01-Intro-to-Node-Streams-and-Utils/breeds.js
+++ 01-Intro-to-Node-Streams-and-Utils/breeds.js
@@ -1,9 +1,9 @@
 const breeds = [
-    { id: 1, name: "Bombay Cat" },
-    { id: 2, name: "Siamese Cat" },
-    { id: 3, name: "Persian Cat" },
-    { id: 4, name: "Maine Coon" },
-    { id: 5, name: "Sphynx" }
+    { id: '1', name: "Bombay Cat" },
+    { id: '2', name: "Siamese Cat" },
+    { id: '3', name: "Persian Cat" },
+    { id: '4', name: "Maine Coon" },
+    { id: '5', name: "Sphynx" }
 ]
 
 export default breeds;
