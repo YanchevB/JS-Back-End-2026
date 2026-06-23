c7985a1f7dee7ec7792755f49f9fa0ebf80aba29
Boyan Yanchev <byanchev6@gmail.com>
2026-06-23 22:20:24 +0300

    fix: restructure repo

--- index.js
+++ index.js
@@ -5,21 +5,31 @@ import { addBreed, readBreeds } from './breedsService.js';
 
 const server = http.createServer(async (req, res) => {
 
+    console.log(cats);
+
     //POST Requests
     if (req.method === 'POST' && req.url === '/cats/add-breed') {
-        let body = '';
-        req.on('data', (chunk) => {
-            body += chunk;
-        });
-
-        req.on('end', (chunk) => {
-            const formData = new URLSearchParams(body);
-            const breedName = formData.get('breed');
-            addBreed(breedName);
-        })
+        const bodyFormData = await readBodyFormData(req); 
+        
+        addBreed(bodyFormData.get('breed'));
 
         //Redirect to home page
         return res.writeHead(302, { Location: '/' }).end();
+    }
+
+    if (req.method === 'POST' && req.url === '/cats/add-cat') {
+        const bodyFormData = await readBodyFormData(req);
+
+        const newCat = {
+            name: bodyFormData.get('name'),
+            description: bodyFormData.get('description'),
+            imageUrl: bodyFormData.get('imageUrl'),
+            breed: bodyFormData.get('breed')
+        }
+
+        cats.push(newCat);
+
+        return res.writeHead(302, { Location: '/'} ).end();
     }
 
     //GET Requests
@@ -48,41 +58,57 @@ const server = http.createServer(async (req, res) => {
     res.end();
 });
 
-server.listen(3000, () => {
-    console.log('Server is listening on http://localhost:3000...');
-});
 
 async function renderAddCatPage() {
     const htmlContent = await fs.readFile('./views/addCat.html', 'utf-8');
-
+    
     const breedOptions = readBreeds().map(breed => `<option value="${breed.id}">${breed.name}</option>`).join('\n');
     const result = htmlContent.replace('{{breedOptions}}', breedOptions);
-
+    
     return result;
 }
 
 async function renderAddBreedPage() {
     const htmlContent = await fs.readFile('./views/addBreed.html', 'utf-8');
-
+    
     return htmlContent;
 }
 
 async function renderHomePage() {
     let htmlContent = await fs.readFile('./views/home/index.html', 'utf-8');
-
+    
     const catTemplate = (cat) => `
-        <li>
-            <img src="${cat.imageUrl}" alt="${cat.name}">
-            <h3>${cat.name}</h3>
-            <p><span>Breed: </span>${cat.breed}</p>
-            <p><span>Description: </span>${cat.description}</p>
-            <ul class="buttons">
-                <li class="btn edit"><a href="">Change Info</a></li>
-                <li class="btn delete"><a href="">New Home</a></li>
-            </ul>
-        </li>`;
-
+    <li>
+    <img src="${cat.imageUrl}" alt="${cat.name}">
+    <h3>${cat.name}</h3>
+    <p><span>Breed: </span>${cat.breed}</p>
+    <p><span>Description: </span>${cat.description}</p>
+    <ul class="buttons">
+    <li class="btn edit"><a href="">Change Info</a></li>
+    <li class="btn delete"><a href="">New Home</a></li>
+    </ul>
+    </li>`;
+    
     htmlContent = htmlContent.replace('{{cats}}', cats.map(cat => catTemplate(cat)).join('\n'));
-
+    
     return htmlContent;
 }
+
+async function readBodyFormData(req) {
+    return new Promise((resolve, reject) => {
+        let body = '';
+
+        req.on('data', (chunk) => {
+            body += chunk;
+        });
+
+        req.on('end', (chunk) => {
+            const formData = new URLSearchParams(body);
+            resolve(formData);
+        });
+    });
+}
+
+server.listen(3000, () => {
+    console.log('Server is listening on http://localhost:3000...');
+});
